fcfccabbb3ce098b78422649460c91000cf3bc1b
Garrett <garrett.smestad@gmail.com>
2016-06-14 11:15:39 -0600

    kg_participation_against_hs_graduation method & tests complete

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -2,33 +2,42 @@ require 'pry'
 require_relative 'district_repository'
 
 class HeadcountAnalyst
-  attr_reader :district_repo
+  attr_reader :district_hash
 
   def initialize(district_repo)
-    @district_repo = district_repo
+    @district_hash = district_repo.district_collection
   end
 
   def kindergarten_participation_rate_variation(district1, district2)
-    district1_average = find_average(district1)
+    district1_average = find_average(district1, :kindergarten_participation)
     district2_name = district2[:against]
-    district2_average = find_average(district2_name)
+    district2_average = find_average(district2_name, :kindergarten_participation)
 
-    (district1_average / district2_average).round(3)
+    truncate(district1_average / district2_average)
   end
 
-  def find_average(district_name)
-    participation = participation_rates_per_year(district_name)
+  def graduation_rate_variation(district1, district2)
+    district1_average = find_average(district1, :high_school_graduation)
+    district2_name = district2[:against]
+    district2_average = find_average(district2_name, :high_school_graduation)
+
+    truncate(district1_average / district2_average)
+  end
+
+  def find_average(district_name, grade_level)
+    participation = participation_rates_per_year(district_name, grade_level)
     participation_sum = participation.values.reduce(0, :+)
     participation_average = participation_sum / participation.keys.count
   end
 
-  def participation_rates_per_year(district_name)
-    district_repo.find_by_name(district_name).enrollment.enrollment_data[:kindergarten_participation]
+  def participation_rates_per_year(district_name, grade_level)
+    district_hash[district_name].enrollment.enrollment_data[grade_level]
   end
 
   def kindergarten_participation_rate_variation_trend(district1, district2)
-    original = participation_rates_per_year(district1).sort.to_h
-    comparison = participation_rates_per_year(district2[:against]).sort.to_h
+    original = participation_rates_per_year(district1, :kindergarten_participation).sort.to_h
+    comparison = participation_rates_per_year(district2[:against], :kindergarten_participation).sort.to_h
+
     calculate_annual_variations(original, comparison)
   end
 
@@ -36,8 +45,22 @@ class HeadcountAnalyst
     result = {}
     # binding.pry
     original.each_key do |key|
-      result[key] = (original[key] / comparison[key]).to_s[0..4].to_f
+      result[key] = truncate(original[key] / comparison[key])
     end
     result
+  end
+
+  def kindergarten_participation_against_high_school_graduation(district_name)
+    kindergarten_variation = kindergarten_participation_rate_variation(district_name, :against=>'COLORADO')
+    graduation_variation = graduation_rate_variation(district_name, :against=>'COLORADO')
+
+    truncate(kindergarten_variation / graduation_variation)
+  end
+
+  def kindergarten_participation_correlates_with_high_school_graduation(name)
+  end
+
+  def truncate(number)
+    number.to_s[0..4].to_f
   end
 end
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -3,17 +3,6 @@ require './lib/headcount_analyst'
 
 class HeadcountAnalystTest < Minitest::Test
 
-  def test_it_initializes_with_a_district_repository
-    dr = DistrictRepository.new
-    dr.load_data({
-      :enrollment => {
-        :kindergarten => "./data/Kindergartners in full-day program.csv"
-      }
-    })
-    ha = HeadcountAnalyst.new(dr)
-    assert_instance_of DistrictRepository, ha.district_repo
-  end
-
   def test_compare_average_participation_between_districts
     dr = DistrictRepository.new
     dr.load_data({
@@ -54,5 +43,33 @@ class HeadcountAnalystTest < Minitest::Test
                2014=>0.661
               }
     assert_equal result, variation_trend
+  end
+
+  def test_compare_kg_participation_to_hs_graduation
+    dr = DistrictRepository.new
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv",
+        :high_school_graduation => "./data/High school graduation rates.csv"
+      }
+    })
+    ha = HeadcountAnalyst.new(dr)
+
+    comparison = ha.kindergarten_participation_against_high_school_graduation('ACADEMY 20')
+    assert_equal 0.641, comparison
+  end
+
+  def test_determine_correlation_between_kg_participation_and_hs_graduation
+    dr = DistrictRepository.new
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv",
+        :high_school_graduation => "./data/High school graduation rates.csv"
+      }
+    })
+    ha = HeadcountAnalyst.new(dr)
+
+    comparison = ha.kindergarten_participation_correlates_with_high_school_graduation(for: 'ACADEMY 20')
+    assert comparison
   end
 end
