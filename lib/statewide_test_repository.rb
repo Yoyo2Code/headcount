2ff7a7f5dd754d28a43ae5dd0f4066602587e52a
Garrett <garrett.smestad@gmail.com>
2016-06-15 13:44:27 -0600

    edits made

--- lib/statewide_test_repository.rb
+++ lib/statewide_test_repository.rb
@@ -2,6 +2,11 @@ require_relative 'data_loader'
 require 'pry'
 
 class StatewideTestRepository
+  attr_reader :statewide_test_objects
+
+  def initialize
+    @statewide_test_objects = {}
+  end
 
   def load_data(file_tree)
     loader = DataLoader.new
@@ -11,6 +16,17 @@ class StatewideTestRepository
     math_by_ethnicity = loader.load_data(:ethnicity, file_tree[:statewide_testing][:math])
     reading_by_ethnicity = loader.load_data(:ethnicity, file_tree[:statewide_testing][:reading])
     writing_by_ethnicity = loader.load_data(:ethnicity, file_tree[:statewide_testing][:writing])
+    third_grade_hashes.each_key do |district_name|
+      statewide_test = StatewideTest.new({
+            :name => district_name,
+            :third_grade => third_grade_data[district_name],
+            :eighth_grade => eighth_grade_data[district_name],
+            :math => math_by_ethnicity[district_name],
+            :reading => reading_by_ethnicity[district_name],
+            :writing => writing_by_ethnicity[district_name]
+        })
+      statewide_test_objects[district_name] = statewide_test
+    end
   end
 
   #   if file_tree[:enrollment][:high_school_graduation].nil? == false
