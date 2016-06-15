0ef1fa8b019de00a5e2b3d7b70891b6e3f2d55bc
Garrett <garrett.smestad@gmail.com>
2016-06-14 18:29:11 -0600

    Started iteration 3. Created classes & tests.

---
+++ test/statewide_test_repository_test.rb
@@ -0,0 +1,19 @@
+require_relative 'test_helper'
+require './lib/statewide_test_repository'
+
+class StatewideTestRepositoryTest < Minitest::Test
+
+  def test_###########
+    str = StatewideTestRepository.new
+    str.load_data({
+      :statewide_testing => {
+        :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
+        :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
+        :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+      }
+    })
+  # str = str.find_by_name("ACADEMY 20")
+  end
+end
---
+++ test/statewide_test_test.rb
@@ -0,0 +1,8 @@
+require_relative 'test_helper'
+require './lib/statewide_test'
+
+class StatewideTestTest < Minitest::Test
+
+  def test_############
+  end
+end
